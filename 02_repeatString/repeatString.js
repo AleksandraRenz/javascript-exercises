99c90677fcdec6b54bfadb1bf4da24a6db434a27
AleksandraRenz <aleksandra.renz@gmail.com>
2023-07-01 18:29:20 +0100

    Add final solution for task 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,23 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    
+    if(times>=0){
+        let result=""
+        for (let i=0; i<times; i++)
+        result += word;
+        return result
+    }else{
+        return "ERROR";
+    }
+    if(times===number){
+        const times = Math.floor(Math.random() * number)
+    }
+    if (word==""){
+        return ""
+    }
 };
+
+
+console.log(repeatString('hey', 3))
 
 // Do not edit below this line
 module.exports = repeatString;
